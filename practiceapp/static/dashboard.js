bfb3f634f5842b1ae033135bed35dbcdf0ddaca1
mohdsameer787 <161015980+mohdsameer787@users.noreply.github.com>
2025-03-04 22:22:54 -0500

    Add files via upload

---
+++ practiceapp/static/dashboard.js
@@ -0,0 +1,27 @@
+
+function toggle(){
+    let panal=document.querySelector(".panale")
+    
+
+    if(panal.classList=="new1"){
+        panal.classList.toggle("logodiv2");
+        panal.classList.toggle("panale");
+    }
+    
+    
+    panal.classList.toggle("new1");
+    
+}  
+function toggle2(){
+    let panal=document.querySelector(".panale")
+    
+
+    if(panal.classList=="panale"){
+        panal.classList.toggle("new1");
+    }
+    
+    
+    
+    
+}  
+
